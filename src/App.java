3bfac6d0f4f6bc0a965120231c18d136ed27227a
Rafael <74793855+Expak@users.noreply.github.com>
2023-04-18 14:24:37 -0300

    Melhorando o codigo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Interface de controle remoto");
+        Controle c1 = new Controle();
+
+        
+        c1.abrirMenu();
+
     }
 }
